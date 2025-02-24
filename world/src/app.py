d775de8fb89f20b9fd6ecfb2a4aace0b95d0952f
Scott Rogers <scott@scottandrewrogers.com>
2025-02-24 16:36:55 -0500

    adding pika / rabbitmq example

--- world/src/app.py
+++ world/src/app.py
@@ -1,26 +1,43 @@
 from flask import Flask
 from flask import request
 from datetime import datetime
+import pika
 
 import os
 import logging
 
+import newrelic.agent
+
+# Initialize the New Relic agent
+# Make sure to call this at the start of your application
+newrelic.agent.initialize()
+
 log = logging.getLogger('werkzeug')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
 shard = os.environ.get('SHARD', "SHARD_NOT_SET")
 ecs_cmd = os.environ.get("ECS_CONTAINER_METADATA_URI_V4", "NOT_SET")
+queue_name = os.environ.get('QUEUE', "hello")
+rmq_host = os.environ.get('RMQ_HOST', "[HOST NOT SET")
 
 def logit(message):
     timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
     log.info(timeString + " - [world: " + shard + "] - " + message)
-    print(timeString + " - [world: " + shard + "] - " + message)
+    # print(timeString + " - [world: " + shard + "] - " + message)
 
 
 @app.route("/")
-def hello_world():
-    print("SHARD: " + shard )
+def world():
+    connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=rmq_host))
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name)
+    log_message = {"message": "Hello World"}
+    context = newrelic.agent.get_linking_metadata()
+    log_message.update(context)
+    logit(str(log_message))
+    channel.basic_publish(exchange='', routing_key='hello', body=str(log_message))
     logit("SHARD: " + shard )
     return "World (" + shard + "), ECS_CONTAINER_METADATA_URI_V4: " + ecs_cmd
