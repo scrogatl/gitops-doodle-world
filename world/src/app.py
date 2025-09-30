733095d2f6d0b9898455628d49989412c8502ef5
scrogatl <scott@scottandrewrogers.com>
2025-09-30 10:23:17 -0400

    added prometheus metrics

--- world/src/app.py
+++ world/src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request
 from datetime import datetime
+from prometheus_client import Counter, Gauge, start_http_server
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
 # import pika
 
 import os
@@ -21,6 +24,22 @@ shard = os.environ.get('SHARD', "SHARD_NOT_SET")
 ecs_cmd = os.environ.get("ECS_CONTAINER_METADATA_URI_V4", "NOT_SET")
 queue_name = os.environ.get('QUEUE', "hello")
 rmq_host = os.environ.get('RMQ_HOST', "[HOST NOT SET")
+
+# Create a Prometheus Counter metric to track requests to the /ping endpoint
+REQUESTS_COUNT = Counter(
+    "doodle_world_requests_total", "Total number of requests to the / endpoint"
+)
+
+# Expose the metrics on a dedicated port (e.g., 8000)
+# The metrics will be available at http://localhost:8000/
+#start_http_server(8000)
+#log.info("started prometheus") 
+
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
 
 def logit(message):
     timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
@@ -47,4 +66,6 @@ def world():
     # Publish to RabbitMQ
     # channel.basic_publish(exchange='', routing_key=queue_name, body=str(log_message))
     
+
+    REQUESTS_COUNT.inc()  # Increment the counter on each request
     return "World (" + shard + "), ECS_CONTAINER_METADATA_URI_V4: " + ecs_cmd
